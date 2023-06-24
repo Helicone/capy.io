2a41198e7b4b9d3b45a9e429ecc0e762c8380a78
Khoa Nguyen <scottmktn@gmail.com>
2023-06-24 15:09:49 -0700

    landing

---
+++ web/pages/signup.tsx
@@ -0,0 +1,13 @@
+interface SignUpProps {}
+
+const SignUp = (props: SignUpProps) => {
+  const {} = props;
+
+  return (
+    <>
+      <h1>Hello SignUp</h1>
+    </>
+  );
+};
+
+export default SignUp;
