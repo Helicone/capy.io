7020c38f327832373dd7a1d4059c3fe0874b91c1
colegottdank <colegottdank@gmail.com>
2023-06-24 23:13:08 -0700

    Update prompt

--- web/lib/getCapyRec.ts
+++ web/lib/getCapyRec.ts
@@ -28,9 +28,7 @@ interface CapyRec {
   title: string;
   imbdId: string;
 }
-export async function getMovieRatings(
-  userIds: string[]
-): Promise<CapyMovieRec[]> {
+export async function getMovieRatings(userIds: string[]): Promise<CapyMovieRec[]> {
   console.log("getMovieRatings started"); // Added console log
   try {
     const configuration = new Configuration({
@@ -42,6 +40,7 @@ export async function getMovieRatings(
     const client = await db.connect();
     console.log("Database client connected"); // Added console log
 
+    console.log(`UserIDs passed in: ${userIds}`); // Added console log
     const reviews = await fetchUserReviews(userIds, client);
     console.log("Fetched user reviews:", reviews); // Added console log
 
@@ -54,10 +53,7 @@ export async function getMovieRatings(
     const movieReviews = formatGroupedRatingsForOpenAI(groupedRatings);
     console.log("Formatted grouped ratings for OpenAI:", movieReviews); // Added console log
 
-    const recommendations = await fetchOpenAIRecommendations(
-      movieReviews,
-      configuration
-    );
+    const recommendations = await fetchOpenAIRecommendations(movieReviews, configuration);
     console.log("Fetched recommendations from OpenAI:", recommendations); // Added console log
 
     const formattedMovies = await formatMovieRecommendations(recommendations);
@@ -70,9 +66,7 @@ export async function getMovieRatings(
   }
 }
 
-async function formatMovieRecommendations(
-  capyRecs: CapyRec[]
-): Promise<CapyMovieRec[]> {
+async function formatMovieRecommendations(capyRecs: CapyRec[]): Promise<CapyMovieRec[]> {
   const recommendationPromises = capyRecs.map(async (rec) => {
     try {
       const { imbdId, title } = rec;
@@ -92,17 +86,13 @@ async function formatMovieRecommendations(
   return Promise.all(recommendationPromises);
 }
 
-async function fetchMovieInformation(
-  imdbId: string
-): Promise<{ posterUrl: string; description: string }> {
+async function fetchMovieInformation(imdbId: string): Promise<{ posterUrl: string; description: string }> {
   try {
     const response = await api.v3.find.findById(imdbId, {
       external_source: "imdb_id",
     });
     return {
-      posterUrl:
-        "https://image.tmdb.org/t/p/w780" +
-        response.movie_results[0].poster_path,
+      posterUrl: "https://image.tmdb.org/t/p/w780" + response.movie_results[0].poster_path,
       description: response.movie_results[0].overview,
     };
   } catch (error) {
@@ -114,14 +104,8 @@ async function fetchMovieInformation(
 function groupRatingsByUser(
   reviews: Review[],
   unseenMovies: UnseenMovie[]
-): Record<
-  string,
-  { preferred: string[]; unpreferred: string[]; notSeen: string[] }
-> {
-  const groupedRatings: Record<
-    string,
-    { preferred: string[]; unpreferred: string[]; notSeen: string[] }
-  > = {};
+): Record<string, { preferred: string[]; unpreferred: string[]; notSeen: string[] }> {
+  const groupedRatings: Record<string, { preferred: string[]; unpreferred: string[]; notSeen: string[] }> = {};
 
   for (const review of reviews) {
     if (!review.user_id) continue; // Skip if user_id is null
@@ -157,13 +141,12 @@ function groupRatingsByUser(
   return groupedRatings;
 }
 
-function formatGroupedRatingsForOpenAI(
-  groupedRatings: ReturnType<typeof groupRatingsByUser>
-): string {
+function formatGroupedRatingsForOpenAI(groupedRatings: ReturnType<typeof groupRatingsByUser>): string {
   let movieReviews = "";
 
+  let i = 1;
   for (const userId in groupedRatings) {
-    movieReviews += `UserId${userId}\n`;
+    movieReviews += `User ${i}\n Ratings`;
     movieReviews += "Preferred | Unpreferred\n";
     const preferredMovies = groupedRatings[userId].preferred;
     const unpreferredMovies = groupedRatings[userId].unpreferred;
@@ -182,15 +165,13 @@ function formatGroupedRatingsForOpenAI(
     }
 
     movieReviews += "\n";
+    i++;
   }
 
   return movieReviews;
 }
 
-async function fetchOpenAIRecommendations(
-  movieReviews: string,
-  configuration: Configuration
-) {
+async function fetchOpenAIRecommendations(movieReviews: string, configuration: Configuration) {
   try {
     const openai = new OpenAIApi(configuration);
     let completion = await openai.createChatCompletion({
@@ -200,25 +181,22 @@ async function fetchOpenAIRecommendations(
           role: "system",
           content: capyRecSystemPrompt,
         },
-        { role: "user", content: `${movieReviews}` },
+        { role: "user", content: `Give 2 movie recommendations for the following user(s): ${movieReviews}` },
       ],
     });
 
+    console.log("OpenAI completion:", completion); // Added console log
     const content = completion.data.choices[0].message?.content!;
-    const jsonSubstring = content.substring(
-      content.indexOf("["),
-      content.lastIndexOf("]") + 1
-    );
+    const jsonSubstring = content.substring(content.indexOf("["), content.lastIndexOf("]") + 1);
+
+    console.log("JSON substring:", jsonSubstring); // Added console log
 
     try {
       // Try parsing the JSON
       return JSON.parse(jsonSubstring);
     } catch (parsingError) {
       // If parsing fails, use GPT-3.5-turbo to fix the JSON
-      console.error(
-        "Error parsing JSON, trying to fix with GPT-3.5-turbo:",
-        parsingError
-      );
+      console.error("Error parsing JSON, trying to fix with GPT-3.5-turbo:", parsingError);
 
       const fixedJSONCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
@@ -241,18 +219,14 @@ async function fetchOpenAIRecommendations(
         ],
       });
 
-      const fixedJSONContent =
-        fixedJSONCompletion.data.choices[0].message?.content!;
+      const fixedJSONContent = fixedJSONCompletion.data.choices[0].message?.content!;
 
       try {
         // Try parsing the corrected JSON
         return JSON.parse(fixedJSONContent);
       } catch (secondParsingError) {
         // If parsing the corrected JSON fails as well
-        console.error(
-          "Error parsing corrected JSON from GPT-3.5-turbo:",
-          secondParsingError
-        );
+        console.error("Error parsing corrected JSON from GPT-3.5-turbo:", secondParsingError);
         throw new Error("JSON parsing error after correction attempt");
       }
     }
@@ -262,32 +236,35 @@ async function fetchOpenAIRecommendations(
   }
 }
 
-async function fetchUserReviews(
-  userIds: string[],
-  client: VercelPoolClient
-): Promise<Review[]> {
-  const placeholders = userIds.map(() => "?").join(",");
+async function fetchUserReviews(userIds: string[], client: VercelPoolClient): Promise<Review[]> {
   try {
-    const queryResult =
-      await client.sql<Review>`SELECT * FROM reviews WHERE user_id IN (${placeholders})`;
-    return queryResult.rows;
+    const reviews: Review[] = [];
+
+    for (const userId of userIds) {
+      const queryResult = await client.sql<Review>`SELECT * FROM reviews WHERE user_id = ${userId}`;
+      reviews.push(...queryResult.rows);
+    }
+
+    return reviews;
   } catch (error) {
     console.error("Error fetching reviews from database:", error);
     throw new Error("Database error");
   }
 }
 
-async function fetchUnseenMovies(
-  userIds: string[],
-  client: VercelPoolClient
-): Promise<UnseenMovie[]> {
-  const placeholders = userIds.map(() => "?").join(",");
+async function fetchUnseenMovies(userIds: string[], client: VercelPoolClient): Promise<UnseenMovie[]> {
   try {
-    const queryResponse =
-      await client.sql<UnseenMovie>`SELECT user_id, movie FROM user_to_movie_meta WHERE has_seen = false and user_id IN (${placeholders})`;
-    return queryResponse.rows;
+    const unseenMovies: UnseenMovie[] = [];
+
+    for (const userId of userIds) {
+      const queryResponse = await client.sql<UnseenMovie>`SELECT user_id, movie FROM user_to_movie_meta WHERE has_seen = false and user_id = ${userId}`;
+      unseenMovies.push(...queryResponse.rows);
+    }
+
+    return unseenMovies;
   } catch (error) {
     console.error("Error fetching unseen movies from database:", error);
     throw new Error("Database error");
   }
 }
+
