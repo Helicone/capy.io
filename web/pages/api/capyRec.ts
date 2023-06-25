5f81ce19914eb899f5cbb6b5a3288b80d3d89fdf
colegottdank <colegottdank@gmail.com>
2023-06-25 11:55:13 -0700

    log error

--- web/pages/api/capyRec.ts
+++ web/pages/api/capyRec.ts
@@ -6,9 +6,7 @@ type CapyRecRequest = {
   usernames: string[];
 };
 
-export type CapyRecResponse =
-  | { capyMovieRecs: CapyMovieRec[] }
-  | { error: string };
+export type CapyRecResponse = { capyMovieRecs: CapyMovieRec[] } | { error: string };
 
 export interface CapyMovieRec {
   imdbID: string;
@@ -17,10 +15,7 @@ export interface CapyMovieRec {
   description: string;
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<CapyRecResponse>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<CapyRecResponse>) {
   console.log("Handler started"); // Added console log
   try {
     const { userId } = getAuth(req);
@@ -36,9 +31,7 @@ export default async function handler(
       console.log("Usernames:", usernames); // Added console log
       let userIds: string[];
       try {
-        userIds = (
-          await clerkClient.users.getUserList({ username: usernames })
-        ).map((user) => user.id);
+        userIds = (await clerkClient.users.getUserList({ username: usernames })).map((user) => user.id);
         console.log("User IDs:", userIds); // Added console log
       } catch (error) {
         console.error("Error fetching user list:", error); // Added console log
@@ -63,16 +56,13 @@ export default async function handler(
 
     console.log("Unsupported request method"); // Added console log
     return res.status(404).json({
-      error:
-        "Method not found for movieRating, only POST works dog come on do better.",
+      error: "Method not found for movieRating, only POST works dog come on do better.",
     });
   } catch (error) {
     // Handle any unexpected errors
     console.error("An unexpected error occurred:", error); // Added console log
     return res.status(500).json({
-      error: `An unexpected error occurred: ${
-        error || "Internal Server Error"
-      }`,
+      error: `An unexpected error occurred: ${JSON.stringify(error)} ${error || "Internal Server Error"}`,
     });
   }
 }
