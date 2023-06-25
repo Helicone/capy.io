355c3225d03c0be7f95a7a5ab97f0049c9205cdb
colegottdank <colegottdank@gmail.com>
2023-06-24 20:20:26 -0700

    Add logs

--- web/pages/api/capyRec.ts
+++ web/pages/api/capyRec.ts
@@ -16,8 +16,10 @@ export interface CapyMovieRec {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<CapyRecResponse>) {
+  console.log("Handler started"); // Added console log
   try {
     const { userId } = getAuth(req);
+    console.log("User ID:", userId); // Added console log
     if (!userId) {
       return res.status(401).json({
         error: "Unauthorized",
@@ -26,10 +28,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     if (req.method === "POST") {
       const { usernames } = req.body as CapyRecRequest;
+      console.log("Usernames:", usernames); // Added console log
       let userIds: string[];
       try {
         userIds = (await clerkClient.users.getUserList({ username: usernames })).map((user) => user.id);
+        console.log("User IDs:", userIds); // Added console log
       } catch (error) {
+        console.error("Error fetching user list:", error); // Added console log
         return res.status(500).json({
           error: "Failed to fetch user list.",
         });
@@ -40,7 +45,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       let capyMovieRecs: CapyMovieRec[];
       try {
         capyMovieRecs = await getMovieRatings(userIds);
+        console.log("Capy movie recommendations:", capyMovieRecs); // Added console log
       } catch (error) {
+        console.error("Error fetching movie ratings:", error); // Added console log
         return res.status(500).json({
           error: "Failed to fetch movie ratings.",
         });
@@ -49,11 +56,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       return res.status(200).json({ capyMovieRecs });
     }
 
+    console.log("Unsupported request method"); // Added console log
     return res.status(404).json({
       error: "Method not found for movieRating, only POST works dog come on do better.",
     });
   } catch (error) {
     // Handle any unexpected errors
+    console.error("An unexpected error occurred:", error); // Added console log
     return res.status(500).json({
       error: `An unexpected error occurred: ${error || "Internal Server Error"}`,
     });
