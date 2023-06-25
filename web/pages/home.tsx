49df7993a26f3d3427bcf7837366431867142bf5
Khoa Nguyen <scottmktn@gmail.com>
2023-06-24 17:55:19 -0700

    quiz

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -1,12 +1,14 @@
-import { User } from "@clerk/nextjs/server";
+import { buildClerkProps, getAuth, User } from "@clerk/nextjs/server";
 import { useUser, SignOutButton } from "@clerk/nextjs";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import BasePage from "@/components/basePage";
 
 interface HomeProps {
   user: User;
 }
 
 const Home = (props: HomeProps) => {
-  //   const { user } = props;
   const { isLoaded, isSignedIn, user } = useUser();
 
   if (!isLoaded || !isSignedIn) {
@@ -14,46 +16,42 @@ const Home = (props: HomeProps) => {
   }
 
   return (
-    <div className="h-screen flex flex-col items-center bg-primary divide-y divide-gray-500">
-      <nav className="w-full p-4 flex flex-row items-center justify-between">
-        <p className="text-white font-semibold text-2xl font-mono tracking-tight">
-          Capybara Recs
-        </p>
-        <button className="bg-accent hover:bg-violet-700 px-4 py-2 text-white rounded-xl font-medium text-md">
-          <SignOutButton />
-        </button>
-      </nav>
-      <main className={`flex flex-col h-full w-full px-4 space-y-8`}>
-        <p className="text-gray-400 font-semibold text-2xl py-8">
-          Hello, <span className="text-white">{user.username}</span> welcome to
-          Capybara Recs!
-        </p>
-        <div className="flex flex-wrap gap-4">
-          <button className="flex flex-col p-4 w-80 rounded-xl h-fit border border-gray-300 text-left space-y-4">
-            <p className="text-white font-semibold text-2xl font-mono tracking-tight">
-              Movie Quiz
-            </p>
-            <p className="text-gray-400 font-semibold text-md">
-              Take a quiz to to build out your movie profile.
-            </p>
-            <div className="w-full flex justify-end">
-              <p className="text-gray-200 text flex-end pt-8">{`< 2 minutes`}</p>
-            </div>
-          </button>
-          <button className="flex flex-col p-4 w-80 rounded-xl h-fit border border-gray-300 text-left space-y-4">
-            <p className="text-white font-semibold text-2xl font-mono tracking-tight">
-              Recommendation w/ friend
-            </p>
-            <p className="text-gray-400 font-semibold text-md">
-              Find a movie recommendation with a friend.
-            </p>
-            <div className="w-full flex justify-end">
-              <p className="text-gray-200 text flex-end pt-8">{`< 2 minutes`}</p>
-            </div>
-          </button>
-        </div>
-      </main>
-    </div>
+    <BasePage>
+      <p className="text-gray-400 font-semibold text-2xl py-8">
+        Hello, <span className="text-white">{user.username}</span> welcome to
+        Capybara Recs!
+      </p>
+      <div className="flex flex-wrap gap-4">
+        <Link
+          href="/quiz"
+          className="flex flex-col p-4 w-80 rounded-xl h-fit border border-gray-300 text-left space-y-4 hover:bg-gray-700"
+        >
+          <p className="text-white font-semibold text-2xl font-mono tracking-tight">
+            Movie Quiz
+          </p>
+          <p className="text-gray-400 font-semibold text-md">
+            Take a quiz to to build out your movie profile.
+          </p>
+          <div className="w-full flex justify-end">
+            <p className="text-gray-200 text flex-end pt-8">{`< 2 minutes`}</p>
+          </div>
+        </Link>
+        <Link
+          href="/recs"
+          className="flex flex-col p-4 w-80 rounded-xl h-fit border border-gray-300 text-left space-y-4 hover:bg-gray-700"
+        >
+          <p className="text-white font-semibold text-2xl font-mono tracking-tight">
+            Recommendation w/ friend
+          </p>
+          <p className="text-gray-400 font-semibold text-md">
+            Find a movie recommendation with a friend.
+          </p>
+          <div className="w-full flex justify-end">
+            <p className="text-gray-200 text flex-end pt-8">{`< 2 minutes`}</p>
+          </div>
+        </Link>
+      </div>
+    </BasePage>
   );
 };
 
---
+++ web/pages/quiz.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @next/next/no-img-element */
+import BasePage from "@/components/basePage";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+
+interface QuizProps {}
+
+const Quiz = (props: QuizProps) => {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
+  return (
+    <BasePage>
+      <div className="flex flex-col text-center justify-center items-center space-y-8 w-full max-w-4xl mx-auto">
+        <p className="text-white font-semibold text-2xl py-8">
+          Click on which movie you think is better!
+        </p>
+        <div className="w-full flex flex-row gap-8 h-full">
+          <div className="w-[50%] text-white hover:text-violet-500 flex flex-col divide-y-2 divide-gray-200 hover:divide-violet-500 items-center border-2 p-0.5 border-gray-200 hover:border-violet-500 hover:cursor-pointer rounded-xl">
+            <img
+              src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+              alt="Movie 1"
+              className="rounded-t-lg"
+              style={{
+                objectFit: "cover",
+              }}
+            />
+            <div className="w-full flex-1 py-4 items-center flex flex-col justify-center bg-gray-600 rounded-b-lg">
+              <p className=" text-lg tracking-wide">Image Title</p>
+            </div>
+          </div>
+          <div className="w-[50%] text-white hover:text-violet-500 flex flex-col divide-y-2 divide-gray-200 hover:divide-violet-500 items-center border-2 p-0.5 border-gray-200 hover:border-violet-500 hover:cursor-pointer rounded-xl">
+            <img
+              src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+              alt="Movie 1"
+              className="rounded-t-lg"
+              style={{
+                objectFit: "cover",
+              }}
+            />
+            <div className="w-full flex-1 py-4 items-center flex flex-col justify-center bg-gray-600 rounded-b-lg">
+              <p className="text-lg tracking-wide">Image Title</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button className="bg-accent hover:bg-violet-700 px-4 py-2 text-white rounded-xl font-medium text-md">
+            Refresh
+          </button>
+        </div>
+      </div>
+    </BasePage>
+  );
+};
+
+export default Quiz;
